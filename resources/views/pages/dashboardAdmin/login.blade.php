1ae76f500b1bed5477a43be1b6d27a4228db3cc5
@brillianita <brillianitaaa@gmail.com>
2021-12-13 00:33:19 +0800

    Memperbaiki tampilan login

--- resources/views/pages/dashboardAdmin/login.blade.php
+++ resources/views/pages/dashboardAdmin/login.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="../assets/stylesheet/style.css" type="text/css">
+    <link rel="stylesheet" href="{{URL::asset('/dist/css/login.css') }}" type="text/css">
 
     <!-- font awesome -->
     <script src="https://kit.fontawesome.com/54218bd639.js" crossorigin="anonymous"></script>
@@ -17,7 +17,7 @@
   <div class="wrapper-login mx-auto">
     <div class="login-title">
         <div class="title-img">
-          <img src="../assets/image/login.png" alt="Scout group" />
+          <img src="../dist/img/login.png" alt="Scout group" />
         </div>
         <div class="title-text">
           <h1>PARAGA</h1>
